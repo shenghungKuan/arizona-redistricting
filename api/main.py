19866e82c7f882cd4ae24dd991bffb9929ab7ef0
marcus0329 <skuan@dons.usfca.edu>
2025-05-07 16:25:56 -0700

    Clean unused

--- api/main.py
+++
@@ -1,9 +0,0 @@
-from typing import Union
-
-from fastapi import FastAPI
-
-from .routers.arizona import arizona_route as arizona
-
-app = FastAPI()
-
-app.include_router(arizona.router)
--- api/routers/arizona/arizona_route.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    prefix="/arizona",
-)
-
-@router.get("/test")
-async def test():
-    return {"message": "This is a test"}
